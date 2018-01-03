e6d5f0c85506a11487b169b37fdf84ccaf2adf48
Phillip Elliott <philliptelliott@gmail.com>
2018-01-02 21:08:05 -0500

    added battle manager

---
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour {
+
+	private enum BattleState{
+		START,
+		PLAYERTURN,
+		ENEMYTURN,
+		WIN,
+		LOSE
+	}
+
+	// Place holder for turn order 
+	private SortedDictionary<int, int> turnOrder;
+
+	private BattleState currentBattleState;
+
+
+	// Use this for initialization
+	void Start() {
+		currentBattleState = BattleState.START;
+		GenerateTurnOrder();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		// Based on state we do something
+		switch (currentBattleState) {
+			case BattleState.START :
+				break;
+			default :
+				break;
+		}
+	}
+
+	private void GenerateTurnOrder() {
+		// Obtain lists of PCs and Enemies
+		// Randomly assign a turn order
+	}
+
+	private void OnTurnEnd() {
+		// Check for win or lose condition
+	}
+}
