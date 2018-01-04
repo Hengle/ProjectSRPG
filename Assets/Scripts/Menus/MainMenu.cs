52dcdec01e833b6bb2fbfe452c9e8e4f6b471edc
Phillip Elliott <philliptelliott@gmail.com>
2018-01-03 21:05:50 -0500

    Added place holders for menus

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+
+	public GameObject MainMenuUI;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Cancel"))
+		{
+			if (GameIsPaused)
+			{
+				closeMainMenu();
+			}
+			else
+			{
+				openMainMenu();
+			}
+		}
+	}
+
+	void openMainMenu()
+	{
+		MainMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+	void closeMainMenu()
+	{
+		MainMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+}
